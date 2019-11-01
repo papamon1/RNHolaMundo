406138e46c12e6e1a66067ccbda1c97f0a3f4efe
JGil <jgil@papamono.es>
2019-11-01 11:01:20 +0100

    Estilos y flex

--- App.js
+++ App.js
@@ -1,19 +1,62 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hola Mundo!</Text>
-    </View>
-  );
+
+class Texto extends React.Component{
+  state={
+    texto: "Hola mundo"
+  };
+
+  handlePress = () => {
+    this.setState({texto:'Adios mundo cruel!'})
+  }
+
+  render(){
+    const { texto } = this.state;
+    return <Text onPress={this.handlePress}>{texto}</Text>;
+  }
+
+};
+
+export default class App extends React.Component {
+  render(){
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>Asd</Text>
+        <Text style={styles.text2}>Asd</Text>
+        <Text style={styles.text3}>Asd</Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
+  text:{
+    flex:1,
+    height:100,
+    width:100,
+    backgroundColor:"brown",
+    color:"red"
+  },
+  text2:{
+    flex:2,
+    height:100,
+    width:100,
+    backgroundColor:"yellow",
+    color:"red"
+  },
+  text3:{
+    flex:3,
+    height:100,
+    width:100,
+    backgroundColor:"orange",
+    color:"red"
+  },
   container: {
     flex: 1,
+    flexDirection: "row",
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: 'flex-start'
+  }
 });
